802a8032aeee3d9d2a7e825cbde19e38779cb087
TP9NS <pshccxx@gmail.com>
2025-05-26 17:41:16 +0900

    first commit

---
+++ backend/shieldus/src/main/java/com/example/shieldus/ShieldusApplication.java
@@ -0,0 +1,13 @@
+package com.example.shieldus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShieldusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShieldusApplication.class, args);
+	}
+
+}

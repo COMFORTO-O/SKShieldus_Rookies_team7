484d3dc75f49e25e3de1b57b85e7f9bb324a09cc
이도건 <rookies.dogun@gmail.com>
2025-05-27 15:49:29 +0900

    Security 설정, JWT 설정, Cookie 등록

---
+++ backend/shieldus/src/main/java/com/example/shieldus/config/controller/TestController.java
@@ -0,0 +1,30 @@
+package com.example.shieldus.config.controller;
+
+
+import com.example.shieldus.entity.member.Member;
+import com.example.shieldus.repository.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    @Transactional
+    @GetMapping("/api/account/login/test/register")
+    public String test() {
+        Member member = Member.builder()
+                .email("test@test.com")
+                .phone("1231231231")
+                .name("test")
+                .password(passwordEncoder.encode("test1234"))
+                .memberRank(10).build();
+        memberRepository.save(member);
+        return "0k";
+    }
+}

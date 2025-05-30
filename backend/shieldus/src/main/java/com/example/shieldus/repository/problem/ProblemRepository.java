e8d6a6fa52378c7ff5d0d618e514ca7536722979
Kwon Soonmin <sogno6037@gmail.com>
2025-05-30 13:42:49 +0900

    백엔드 폴더 삭제

--- backend/shieldus/src/main/java/com/example/shieldus/repository/problem/ProblemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.shieldus.repository.problem;
-
-import com.example.shieldus.entity.problem.Problem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProblemRepository extends JpaRepository<Problem, Long> {
-}

24751b8cc175392afe008232237e51a377ccfd22
이도건 <rookies.dogun@gmail.com>
2025-05-31 16:25:26 +0900

    deleteMember deleteMapping 변경

--- backend/shieldus/src/main/java/com/example/shieldus/controller/MemberController.java
+++ backend/shieldus/src/main/java/com/example/shieldus/controller/MemberController.java
@@ -11,10 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -42,7 +39,7 @@ public class MemberController {
     }
 
     // 사용자 삭제 ( soft ), 진짜 삭제가 아닌 컬럼 붙여서 삭제
-    @GetMapping("/delete")
+    @DeleteMapping("/delete")
     public ResponseDto<String> deleteMember(@AuthenticationPrincipal MemberUserDetails userDetails) {
         memberService.deleteMember(userDetails.getMemberId());
         return ResponseDto.success("ok");
